f117d091a50e6172662694a595d77b4d4b32768b
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-21 18:58:29 +1000

    chore: Enable C# and Razor syntax highlight
    We will be publishing a post related to these two languages soon!

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -115,6 +115,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['csharp', 'cshtml']
     },
   } satisfies Preset.ThemeConfig,
 };
